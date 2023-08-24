9ab2d3608b26ba71b934ba17774014b8f4ab0ed8
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2023-08-24 20:07:21 +0300

    add bootstrap folders

--- app.js
+++ app.js
@@ -84,87 +84,95 @@ heart.forEach(i => {
 //------------------- Shopping Cart ----------------------//
 // const btnAddToCart = document.querySelectorAll('.btn-addToCart'),
 //       cartId = document.getElementById('cartId'),
-const cartContent = document.querySelector('#cartItems');
-const itemAlert = document.querySelector('.itemAlert');
-const basketNum = document.getElementById('basketNum')
+const cartContent = document.getElementById('cartItems'),
+      itemAlert = document.querySelector('.itemAlert'),
+      basketNum = document.getElementById('basketNum'),
+      cartInput = document.querySelectorAll('.cartInput'),
+      cartPlus = document.getElementById('cartPlus'),
+      cartPrice = document.querySelectorAll('.cart-price'),
+      totalPrice = document.getElementById('total-price'),
+      navTotalPrice = document.querySelector('.navTotal-price');
 let cartItems = [];    
+let cartItemsDetails = [];
 
-//---Function to add item to shopping cart
+//--- Function to add item to shopping cart
 const addToCart = (id,img,title,price)=> {
   if(!cartItems.includes(id)) {
-  cartItems.push(id); // Add new item to array
+  // [Add new item as object to array]
+  //cartItems.push({...id,id,title,price});
+  cartItems.push(id);
   let newItem = document.createElement('div');
     newItem.innerHTML =
       `<div id=${id} class="cart-content">
         <div>
           <img src="${img}" class='cart-img' alt="">
           <div class="addToCart-counter position-relative d-flex justify-content-center align-items-center mt-3">
-            <button id="cart-minus" class="btn border">-</button>
-            <input id='cartInput' type="number" value="1" min="0">
-            <button id="cartPlus" class="btn border" onclick='ff()'>+</button>
+            <button class="cart-minus btn border">-</button>
+            <input class='inputCart' type="number" value="1" min="0">
+            <button class="cartPlus btn border">+</button>
           </div>
         </div>
         <div class="detail-box">
           <div class="cart-product-title">${title}</div>
-          <div class="cart-price">${price} $</div>
+          <div class="cart-price">${price}</div><span>$</span>
         </div>
         <!--Remove a product from cart-->
         <i class="fa-solid fa-trash cart-remove" onclick='removeItem(this)'></i>
       </div>`
     cartContent.append(newItem);
-    //popup Alert box
+    cartItemsDetails.push({...id,id,title,price});
+    //[popup Alert box when add new item to the cart]
     itemAlert.classList.add('show');
     setTimeout(() => {
       itemAlert.classList.remove('show');
     }, 2000);
-    ++basketNum.innerHTML; // Increase the number of cart items in basket
+    //[Increase the number of cart items in basket]
+    ++basketNum.innerHTML; 
   } else {
+    //[popup Alert when choose the same item twice]
     Swal.fire('You are already add this item to cart, Increase Your Product Quantity')
     return false;
   }
   updateTotal();
 }
 console.log(cartItems);
+console.log(cartItemsDetails);
 
-//---Function to remove item from shopping cart
+//--- Function to remove item from shopping cart
 const removeItem = (e)=> {
   let delItem = cartItems.indexOf(e.parentElement.id).toString();
   cartItems.splice(delItem,1);
    e.parentElement.remove();
    updateTotal();
-   --basketNum.innerHTML; // Decrease the number of cart items in basket
+   //[Decrease the number of cart items in basket]
+   --basketNum.innerHTML; 
 }
-//---Total Price 
-const shoppingCart = document.getElementById('cartItems');
+
+//--- Function to Handle Total Price 
 const updateTotal = ()=> {
-  const cartPrice = document.querySelectorAll('.cart-price');
-  const totalPrice = document.querySelector('.total-price');
-  const navTotalPrice = document.querySelector('.navTotal-price');
+  const currency = `<span>$</span>`
   let num= [];
-  if (shoppingCart.children.length > 0) {
-    cartPrice.forEach(i => {
-      num.push(Math.floor(i.innerHTML)); //Use Math.floor to change string value to number
+  if (cartContent.children.length > 0) {
+    cartPrice.forEach(cart => {
+      //[Use Math.floor to change string value to number]
+      num.push(Math.floor(cart.innerHTML)); 
+      console.log(num);
     })
   }
-  totalPrice.innerHTML = num.reduce((prev,curr)=> prev+curr,0);
+  totalPrice.innerHTML = num.reduce((prev,curr)=> prev+curr,0)+currency;
   //navTotalPrice.innerHTML = totalPrice.innerHTML;
   //HandlePopupPrice();
 }
-//---Handle Icrease & Decrease Input Number
-const cartInput = document.getElementById('cartInput');
-const cartPlus = document.getElementById('cartPlus');
 
-if (cartContent.hasChildNodes()) {
-  // It has at least one
-  const ff = ()=> {
-    console.log('shopppping')
+//---Function to Handle Icrease & Decrease Input Number
+  //[Check if the shopping card includes items]
+  if (cartContent.hasChildNodes()) {
+    let inputCart = document.querySelector('.inputCart');
+    console.log(inputCart);
+    // cartItems.forEach(cart=> {
+    //   console.log(cart);
+    // })
   }
-}
-
-
-
-
-
 
 //---Handle Total Price Popup 
 // const pricePopup = document.getElementById('popupNavPrice');
@@ -176,7 +184,7 @@ if (cartContent.hasChildNodes()) {
 // }
 
 
-//------------------------ Shopping Card --------------------------//
+//------------------------ Navbar Shopping Basket --------------------------//
 const basket = document.getElementById('basket');
 const cart = document.querySelector('.cart');
 const menuCloseBtn = document.getElementById('menuCloseBtn');
@@ -216,4 +224,5 @@ const menuToggle = document.getElementById('navbarNavDropdown')
 //})
 
 
+//test
 
